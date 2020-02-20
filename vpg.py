2c0077c1538928e6a9d2be89b2260f3261adb19b
Leonardo Barazza <leonardo.barazza@gmail.com>
2020-02-20 20:12:27 +0100

    change vpg.py

--- vpg.py
+++ vpg.py
@@ -7,75 +7,11 @@ import copy
 from collections import deque
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
-
-env = gym.make("Pendulum-v0")
-seed = 1234567#1234567
-#random.seed(seed)
-torch.manual_seed(seed)
-env.seed(seed)
-np.random.seed(seed)
+from models import *
+from memory import Memory
 
 
-class Actor(nn.Module):
-    def __init__(self, nS, nA):
-        super(Actor, self).__init__()
-
-        self.h = nn.Linear(nS, 100)
-        self.i = nn.Linear(100, 100)
-        self.out_mean = nn.Linear(100, nA)
-        self.out_std  = nn.Linear(100, nA)
-
-    def forward(self, x):
-        x = F.relu(self.h(x))
-        x = F.relu(self.i(x))
-        mean = self.out_mean(x)
-        std  = torch.abs(self.out_std(x))
-        return Normal(mean, std)
-
-
-class Critic(nn.Module):
-    def __init__(self, nS):
-        super(Critic, self).__init__()
-
-        self.h = nn.Linear(nS, 100)
-        self.i = nn.Linear(100, 100)
-        self.out = nn.Linear(100, 1)
-
-    def forward(self, x):
-        x = F.relu(self.h(x))
-        x = F.relu(self.i(x))
-        v = self.out(x)
-        return v
-
-class Memory:
-    def __init__(self):
-        self.states = []
-        self.actions = []
-        self.rewards = []
-        self.episode_rewards = []
-        self.T = 0
-
-    def clear(self):
-        self.states = []
-        self.actions = []
-        self.rewards = []
-        self.T = 0
-
-    def add(self, state, action, reward, done):
-        self.T += 1
-        self.states.append(state)
-        self.actions.append(action)
-        self.episode_rewards.append(reward)
-        if done:
-            self.rewards.append(self.episode_rewards)
-            self.episode_rewards = []
-
-    def get(self):
-        states = torch.tensor(self.states).float()
-        actions = torch.tensor(self.actions).float()
-        return states, actions, self.rewards, self.T
-
-class ppo:
+class vpg:
     def __init__(self):
         self.memory = Memory()
         self.policy = Actor(3, 1)
@@ -87,6 +23,7 @@ class ppo:
 
     @torch.no_grad()
     def choose_action(self, state):
+
         return self.policy(state).sample()
 
     def step(self, state, action, reward, done):
@@ -134,45 +71,3 @@ class ppo:
         self.memory.clear()
 
         return vpg_loss, critic_loss, v, actions
-
-tb = SummaryWriter()
-
-agent = ppo()
-
-n_episode = 0
-
-returns = deque(maxlen=100)
-while True:
-    n_episode += 1
-
-    episode_reward = 0
-    state = env.reset()
-    while True:
-        env.render()
-
-        # action needs to be a list since this accepts Box Actions
-        # the reeward is of the same type as the action that we pass in
-
-        action = [agent.choose_action(torch.tensor(state).unsqueeze(0).float()).item()]
-        next_state, reward, done, info = env.step(action) # take a random action
-        agent.step(state, action, reward, done)
-
-        state = next_state
-        episode_reward += reward
-        if done:
-
-            if n_episode%1==0:
-                vpg_loss, critic_loss, v_predicted, actions = agent.update()
-
-                tb.add_scalar("Critic Loss", critic_loss, n_episode)
-                tb.add_histogram("Actions", actions)
-                tb.add_histogram("V-Predicted", v_predicted, n_episode)
-            tb.add_scalar("Rewards", episode_reward, n_episode)
-
-            returns.append(episode_reward)
-            print("Episode n. {:6d}    Return: {:9.2f}  Avg. Return: {:9.2f}".format(n_episode, episode_reward, np.mean(returns)))
-
-            break
-
-env.close()
-tb.close()
