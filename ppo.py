4d61045af7ee0e4e05b4265b04f2acb47da1eefd
Leonardo Barazza <leonardo.barazza@gmail.com>
2020-02-23 10:05:04 +0100

    change variables' names

--- ppo.py
+++ ppo.py
@@ -30,23 +30,23 @@ class ppo:
     def step(self, state, action, reward, done):
         self.memory.add(state, action, reward, done)
 
-    # ( "_" subscript stands for "all of")
     def update(self, batch_size, n_updates, v_updates):
-        states, actions, R_, T_ = self.memory.get()
-        n_ep = len(R_)
+        states, actions, R, T = self.memory.get()
+        n_ep = len(R)
 
-        v_ = self.critic(states)
-        A_, rtg_ = utils.gae_rtg((R_, v_, T_), self.gam, self.lam)
+        v = self.critic(states)
+        A, rtg = utils.gae_rtg((R, v, T), self.gam, self.lam)
         policy_old = copy.deepcopy(self.policy)
-        log_probs_old_ = policy_old(states).log_prob(actions)
+        log_probs_old = policy_old(states).log_prob(actions)
 
-        for (v, A, rtg, log_probs_old), i in utils.sample_batch((v_, A_, rtg_, log_probs_old_), batch_size, n_updates): # 'i' is the index of the sampled items
-            log_probs = self.policy(states).log_prob(actions)[i]
+        # "_" subscript indicates "sampled from"
+        for (v_, A_, rtg_, log_probs_old_), i in utils.sample_batch((v, A, rtg, log_probs_old), batch_size, n_updates): # 'i' is the index of the sampled items
+            log_probs_ = self.policy(states).log_prob(actions)[i]
 
-            r = torch.exp(log_probs - log_probs_old)
+            r_ = torch.exp(log_probs_ - log_probs_old_)
 
-            l_1 = r * A
-            l_2 = torch.clamp(r, 1-self.eps, 1+self.eps) * A
+            l_1 = r_ * A_
+            l_2 = torch.clamp(r_, 1-self.eps, 1+self.eps) * A_
 
             # TODO: implement entropy
             l_clip = -1/n_ep * torch.sum(torch.min(l_1, l_2))
@@ -55,8 +55,9 @@ class ppo:
             l_clip.backward(retain_graph=True)
             self.policy_optimizer.step()
 
-        for (v, rtg), _ in utils.sample_batch((v_, rtg_), batch_size, v_updates):
-            critic_loss = 1/n_ep * F.mse_loss(v, rtg)
+        # FIXME: critic_loss only represents last critic_loss_
+        for (v_, rtg_), _ in utils.sample_batch((v, rtg), batch_size, v_updates):
+            critic_loss = 1/n_ep * F.mse_loss(v_, rtg_)
             self.critic_optimizer.zero_grad()
             critic_loss.backward(retain_graph=True)
             self.critic_optimizer.step()
